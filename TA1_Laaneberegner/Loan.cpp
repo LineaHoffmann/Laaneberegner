45b85865b16b59c6d575ac2cbc3a068fc124f115
Jawar19 <jawar19@student.sdu.dk>
2020-03-12 15:22:52 +0100

    getter/setter

---
+++ TA1_Laaneberegner/Loan.cpp
@@ -0,0 +1,42 @@
+#include <Loan.h>
+
+
+int Loan::getYears() const
+{
+
+}
+
+void Loan::setYears(int years)
+{
+
+}
+
+int Loan::getPaymentsPerYear() const
+{
+
+}
+
+void Loan::setPaymentsPerYear(int paymentsPerYear)
+{
+
+}
+
+double Loan::getDebt() const
+{
+
+}
+
+void Loan::setDebt(double debt)
+{
+
+}
+
+double Loan::getInterestRate() const
+{
+
+}
+
+void Loan::setInterestRate(double rate)
+{
+    mInterestRate = rate;
+}
