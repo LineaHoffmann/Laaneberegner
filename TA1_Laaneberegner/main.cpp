ec35a7dbed8371e7fd894e3a62cf3e3b30fe81b6
Linea <Linea.hoffmann.christophersen@gmail.com>
2020-03-15 15:26:49 +0100

    Lidt main indstillinger

--- TA1_Laaneberegner/main.cpp
+++ TA1_Laaneberegner/main.cpp
@@ -7,9 +7,15 @@ using namespace std;
 int main()
 {
     // Variables for input/output
-    int setYears = 0;
-    std::cin >> setYear;
+    int setYears;
+
     std::cout << "Velkommen til laaneberegneren " << std:: endl;
+    Loan l2;
+    std::cout << "Sæt venligst beløb på lånet, indsæt ikke komma i tallet: ";
+    std::cin >> setYears;
+    std:: cout << " Sæt hvor mange år de ønsker at lånet skal løbe over: " << std::endl;
+
+    std::cin >> setYears;
 
 
     Loan l1(1000000, 30, 4, 3);
