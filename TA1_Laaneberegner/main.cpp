3719369ad0b76cb6f6b8a7dc5c4f4333970dfc24
Linea <Linea.hoffmann.christophersen@gmail.com>
2020-03-15 15:34:53 +0100

    more main work

--- TA1_Laaneberegner/main.cpp
+++ TA1_Laaneberegner/main.cpp
@@ -16,18 +16,12 @@ int main()
     std::cin >> setYears;
     std:: cout << " Sæt hvor mange år de ønsker at lånet skal løbe over: " << std::endl;
 
-    std::cin >> setYears;
-
 
     Loan l1(1000000, 30, 4, 3);
     l1.getInterestRate();
     std::cout << "total payment: " << static_cast<long>(l1.totalPayment()) << " Total Interest: " << l1.totalInterest() << std::endl;
 
     l1.outputPeriodicalPayments(std::cout);
-
-
-
-
 
     return 0;
 }
