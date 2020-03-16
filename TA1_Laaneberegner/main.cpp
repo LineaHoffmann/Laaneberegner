4cef3288825344b60d5bc0b07066f08cf88f70f9
Linea <Linea.hoffmann.christophersen@gmail.com>
2020-03-16 16:09:05 +0100

    Uncommit test stuff main

--- TA1_Laaneberegner/main.cpp
+++ TA1_Laaneberegner/main.cpp
@@ -57,9 +57,9 @@ int main()
 
     //test stuff
     Loan l1(1000000, 30, 4, 3);
-//    l1.getInterestRate();
-//    std::cout << "total payment: " << static_cast<long>(l1.totalPayment()) << " Total Interest: " << l1.totalInterest() << std::endl;
-//    l1.outputPeriodicalPayments(std::cout);
+    l1.getInterestRate();
+    std::cout << "total payment: " << static_cast<long>(l1.totalPayment()) << " Total Interest: " << l1.totalInterest() << std::endl;
+    l1.outputPeriodicalPayments(std::cout);
 
     double loanDetailArr[5]{2.0, 2.6, 2.7, 1.45, 1.7};
 
