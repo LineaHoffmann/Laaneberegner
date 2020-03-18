72fc0d34af9591a85d001a8afb2075190ec2ed4f
Linea <Linea.hoffmann.christophersen@gmail.com>
2020-03-18 10:35:38 +0100

    Rettet til i main

--- TA1_Laaneberegner/main.cpp
+++ TA1_Laaneberegner/main.cpp
@@ -4,66 +4,68 @@
 #include <string>
 
 
-//initializin static variable values of built-in array and added sums of values for the 5 types of loan
-//Array to hold values for different types of loans from https://www.totalkredit.dk/siteassets/dokumenter/privat/prisblad/prisblad.pdf
-double loanDetailArr[5]{2.0, 2.6, 2.7, 1.45, 1.7};
-
 int main()
 {
-//    // Temporary input variable
-//    std::string input;
+    // Temporary input variable
+    std::string input;
 
-//    // Outputs a welcome message to the user
-//    std::cout << "Velkommen til laaneberegneren " << std:: endl;
-//    // Initilize an object, l2 with default contructor
-//    Loan l2;
-
-//    // Outputs message to user, takes input converts input string to double
-//    // and using setter to set membervariable on object l2.
-//    std::cout << "Sæt venligst beløb på lånet, indsæt ikke komma i tallet: ";
-//    std::cin >> input;
-//    l2.setDebt(std::stod(input));
-
-//    // Outputs message to user, takes input converts input string to int
-//    // and using setter to set membervariable on object l2.
-//    std:: cout << "Sæt hvor mange år de ønsker at lånet skal løbe over: ";
-//    std::cin >> input;
-//    l2.setYears(std::stoi(input));
-
-//    // Outputs message to user, takes input converts input string to int
-//    // and using setter to set membervariable on object l2.
-//    std::cout << "Sæt hvor mange terminer de ønsker om året: ";
-//    std::cin >> input;
-//    l2.setPaymentsPerYear(std::stoi(input));
-
-//    // Outputs message to user, takes input converts input string to double
-//    // and using setter to set membervariable on object l2.
-//    std::cout << "Sæt den ønskede rente : " << std::endl;
-//    std::cin >> input;
-//    l2.setInterestRate(std::stod(input));
-
-//    // Calculates the amount the loaner, l2, have to pay per periode.
-//    l2.calcPeriodicFee();
-
-//    //Outputs the interest rate
-//    std::cout << "Lånerenten er: " << input << "% \n"
-//              << " Samlet betaling: "<< static_cast<long>(l2.totalPayment())  << "\n"
-//              <<  " Samlet rentegebyr:  " << std::fixed << l2.totalInterest() << std::endl;
-
-//    // Outputs the interest with tax deduction
-//    std::cout << "Samlet fradrag: " <<l2.totalInterestTaxDeducted(30.6) << std::endl;
-//    //Outputs the Periodical Payments
-//    l2.outputPeriodicalPayments(std::cout);
-
-    //test stuff
-    Loan l1(1000000, 30, 4, 3);
-    l1.getInterestRate();
-    std::cout << "total payment: " << static_cast<long>(l1.totalPayment()) << " Total Interest: " << l1.totalInterest() << std::endl;
-    l1.outputPeriodicalPayments(std::cout);
-
+    //initializin static variable values of built-in array and added sums of values for the 5 types of loan
+    //Array to hold values for different types of loans from https://www.totalkredit.dk/siteassets/dokumenter/privat/prisblad/prisblad.pdf
     double loanDetailArr[5]{2.0, 2.6, 2.7, 1.45, 1.7};
 
-    l1.calcAllLoan(loanDetailArr,_countof(loanDetailArr));
+    // Outputs a welcome message to the user
+    std::cout << "Velkommen til laaneberegneren " << std:: endl;
+    // Initilize an object, l2 with default contructor
+    Loan l2;
+
+    // Outputs message to user, takes input converts input string to double
+    // and using setter to set membervariable on object l2.
+    std::cout << "Saet venligst beloeb paa laanet, indsaet ikke komma i tallet: ";
+    std::cin >> input;
+    l2.setDebt(std::stod(input));
+
+    // Outputs message to user, takes input converts input string to int
+    // and using setter to set membervariable on object l2.
+    std:: cout << "Saet hvor mange aar de oensker at laanet skal loebe over: ";
+    std::cin >> input;
+    l2.setYears(std::stoi(input));
+
+    // Outputs message to user, takes input converts input string to int
+    // and using setter to set membervariable on object l2.
+    std::cout << "Saet hvor mange terminer de oensker om aaret: ";
+    std::cin >> input;
+    l2.setPaymentsPerYear(std::stoi(input));
+
+    // Outputs message to user, takes input converts input string to double
+    // and using setter to set membervariable on object l2.
+    std::cout << "Saet den oenskede rente : ";
+    std::cin >> input;
+    l2.setInterestRate(std::stod(input));
+
+    // Calculates the amount the loaner, l2, have to pay per periode.
+    l2.calcPeriodicFee();
+
+    //Outputs the interest rate
+    std::cout << "Laanerenten er: " << input << "% \n"
+              << " Samlet betaling: "<< static_cast<long>(l2.totalPayment())  << "\n"
+              <<  " Samlet rentegebyr:  " << std::fixed << l2.totalInterest() << std::endl;
+
+    // Outputs the interest with tax deduction
+    std::cout << "Samlet fradrag: " <<l2.totalInterestTaxDeducted(30.6) << std::endl;
+    //Outputs the Periodical Payments
+    l2.outputPeriodicalPayments(std::cout);
+
+    //Outputs loan details for all loan types
+    std::cout << "Beregner for andre laanetyper med hhv. 2% rente, 2,6% rente, 2,7% rente, 1,45% rente, 1.7% rente" << std::endl;
+    l2.calcAllLoan(loanDetailArr,_countof(loanDetailArr));
+
+    //test stuff
+//    Loan l1(1000000, 30, 4, 3);
+//    l1.getInterestRate();
+//    std::cout << "total payment: " << static_cast<long>(l1.totalPayment()) << " Total Interest: " << l1.totalInterest() << std::endl;
+//    l1.outputPeriodicalPayments(std::cout);
+
+//    l1.calcAllLoan(loanDetailArr,_countof(loanDetailArr));
 
     return 0;
 }
