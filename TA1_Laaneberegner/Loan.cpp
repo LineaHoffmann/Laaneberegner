16167499b5d1d5fdf6c47f445661ebd297346a20
Linea <Linea.hoffmann.christophersen@gmail.com>
2020-03-18 11:18:22 +0100

    Added some kind of comment/documentation for functions

--- TA1_Laaneberegner/Loan.cpp
+++ TA1_Laaneberegner/Loan.cpp
@@ -3,12 +3,11 @@
 #include <iomanip>
 #include <math.h>
 
-/*
- * Constructor for the Loan objects.
- * Takes double debt, int years, int paymentYears, double InterestRate
-*/
+//Constructor
 Loan::Loan(){}
 
+// Constructor for the Loan objects.
+// Takes double debt, int years, int paymentYears, double InterestRate
 Loan::Loan(double debt, int years, int paymentsPerYear, double interestRate)
 {
     //calling functions to set creation values
@@ -21,19 +20,16 @@ Loan::Loan(double debt, int years, int paymentsPerYear, double interestRate)
     calcPeriodicFee();
 }
 
-/*
- * Get Years of the loan.
-*/
+
+// Get Years of the loan.
 int Loan::getYears() const
 {
     return mYears;
-
 }
 
-/*
- * Set Years of the loan.
- * Years can't be less than 0 or bigger than 30 years
-*/
+
+// Set Years of the loan.
+// Years can't be less than 0 or bigger than 30 years (normally limited to 1 to 30 years)
 void Loan::setYears(int years)
 {
     if(years > 0 && years <= 30)
@@ -44,12 +40,15 @@ void Loan::setYears(int years)
         std::cout << "Ugyldigt argument" << std::endl;
 }
 
+// Gets payment per year
 int Loan::getPaymentsPerYear() const
 {
     return mPaymentsPerYear;
 
 }
 
+// Sets the payment per year. If user insert invalid number, user gets an error
+// Can't be more than 12, becuase normally pays monthly.
 void Loan::setPaymentsPerYear(int paymentsPerYear)
 {
     if(paymentsPerYear > 0 && paymentsPerYear <= 12)
@@ -58,45 +57,57 @@ void Loan::setPaymentsPerYear(int paymentsPerYear)
         }
     else
     {
-        std::cout << "Ugyldig indtastning, venligst vælg en nummer mellem 1 og 12" << std::endl;
+        std::cout << "Ugyldig indtastning, venligst vaelg en nummer mellem 1 og 12" << std::endl;
     }
 }
 
+// Get the debt
 double Loan::getDebt() const
 {
     return mDebt;
 }
 
+// Set the debt, uses a doube as parameter
 void Loan::setDebt(double debt)
 {
     mDebt = debt;
 }
 
+// Get interest rate
 double Loan::getInterestRate() const
 {
     return mInterestRate;
 }
 
+// Set interest rate, takes double as parameter
+//
 void Loan::setInterestRate(double rate)
 {
    mInterestRate = rate/mPaymentsPerYear;
 }
 
+// calculates the total interest
+// takes payment - debt and returns the result.
 double Loan::totalInterest() const
 {
     return totalPayment() - getDebt();
 }
 
+// calculates total payment
+// takes periodic fee * (years * payments per year) retuns the result
 double Loan::totalPayment() const
 {
          return mperiodicFee * (mYears * mPaymentsPerYear);
 }
 
+// calculates the total interest after tax deduction
+// takes total interest * tac deduction / 100 and substracs from total interest
 double Loan::totalInterestTaxDeducted(double taxDeductionRate) const
 {
-    return totalInterest() * (taxDeductionRate/100);
+    return totalInterest() - (totalInterest() * (taxDeductionRate/100));
 }
 
+// prints out a tableveiw for all the payments of the loan
 void Loan::outputPeriodicalPayments(std::ostream &ost) const
 {
     double tempPV{0}, tempR = mDebt;
@@ -135,12 +146,14 @@ void Loan::outputPeriodicalPayments(std::ostream &ost) const
     }
 }
 
+// calculates the periodic fee per termin
 void Loan::calcPeriodicFee()
 {
     mperiodicFee = mDebt * (mInterestRate/100)/(1 - (1/pow((1 + (mInterestRate/100)), static_cast<long double>(mYears * mPaymentsPerYear))));
 }
 
-
+//Calculates and print loan types given in the array.
+// takes an double array and size_t numberOfElements as parameters.
 void Loan::calcAllLoan(const double array[], const size_t numberOfElements)
 {
     //iterate through array setting intrestrate and calculat total payments ect. and printing.
